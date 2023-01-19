4961af7e2c49ae621855ca22c8eb8471281dcd99
Gohler <gochpatryk@gmail.com>
2023-01-19 15:24:41 +0100

    Add function that allows player to choose between rock, paper and scissors

--- java.js
+++ java.js
@@ -2,3 +2,8 @@ function getComputerChoice(computer) {
     return computer[Math.floor(Math.random()*computer.length)];
 }
 var computer = ["rock", "paper","scissors"]
+
+function getPlayerChoice(player) {
+    return player.toLowerCase()
+}
+var player = prompt("What do you choose? Rock, paper or scissors?");
